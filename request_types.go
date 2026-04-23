e61fb4c21949a15ed032740e94ee8b118cddfe01
Mohd Munazil Bin Mohd Azlan <munazil@Mohds-MacBook-Pro.local>
2026-04-23 17:10:29 +0800

    change list of strings to payment method

--- request_types.go
+++ request_types.go
@@ -1,24 +1,24 @@
 package chippg
 
 type createPurchaseRequest struct {
-	Client                 ClientDetails   `json:"client"`
-	Purchase               PurchaseDetails `json:"purchase"`
-	BrandID                string          `json:"brand_id"`
-	SuccessRedirect        *string         `json:"success_redirect,omitempty"`
-	FailureRedirect        *string         `json:"failure_redirect,omitempty"`
-	CancelRedirect         *string         `json:"cancel_redirect,omitempty"`
-	SuccessCallback        *string         `json:"success_callback,omitempty"`
-	PaymentMethodWhitelist *[]string       `json:"payment_method_whitelist,omitempty"`
+	Client                 ClientDetails    `json:"client"`
+	Purchase               PurchaseDetails  `json:"purchase"`
+	BrandID                string           `json:"brand_id"`
+	SuccessRedirect        *string          `json:"success_redirect,omitempty"`
+	FailureRedirect        *string          `json:"failure_redirect,omitempty"`
+	CancelRedirect         *string          `json:"cancel_redirect,omitempty"`
+	SuccessCallback        *string          `json:"success_callback,omitempty"`
+	PaymentMethodWhitelist *[]PaymentMethod `json:"payment_method_whitelist,omitempty"`
 }
 
 type PurchaseOption struct {
-	Client                 ClientDetails   `json:"client"`
-	Purchase               PurchaseDetails `json:"purchase"`
-	SuccessRedirect        *string         `json:"success_redirect,omitempty"`
-	FailureRedirect        *string         `json:"failure_redirect,omitempty"`
-	CancelRedirect         *string         `json:"cancel_redirect,omitempty"`
-	SuccessCallback        *string         `json:"success_callback,omitempty"`
-	PaymentMethodWhitelist *[]string       `json:"payment_method_whitelist,omitempty"`
+	Client                 ClientDetails    `json:"client"`
+	Purchase               PurchaseDetails  `json:"purchase"`
+	SuccessRedirect        *string          `json:"success_redirect,omitempty"`
+	FailureRedirect        *string          `json:"failure_redirect,omitempty"`
+	CancelRedirect         *string          `json:"cancel_redirect,omitempty"`
+	SuccessCallback        *string          `json:"success_callback,omitempty"`
+	PaymentMethodWhitelist *[]PaymentMethod `json:"payment_method_whitelist,omitempty"`
 }
 
 type ClientDetails struct {
